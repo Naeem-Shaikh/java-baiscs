52cb097f39c8c3f35f09a1c1fba1ad2eff519e36
Naeem Shaikh <naeem5@yahoo.com>
2021-07-15 13:14:15 -0400

    Arrays with objects

--- src/ArraysDemo/ArraysDemo.java
+++ src/ArraysDemo/ArraysDemo.java
@@ -10,12 +10,25 @@ public class ArraysDemo {
 //        scores[1] = 70;
 //        scores[2] = 80;
 //        scores[3]= 100;
-   //      int[] scores = new int[]{90,80,70,100}; // method 2
-        int[] scores = {90,80,70,100}; // method 3
+        //      int[] scores = new int[]{90,80,70,100}; // method 2
+        int[] scores = {90, 80, 70, 100}; // method 3
 
-        for (int score: scores) {
+        for (int score : scores) {
             System.out.println(score);
         }
-        System.out.println("Size of Array: "+scores.length);
+        System.out.println("Size of Array: " + scores.length);
+
+//        Student[] students = new Student[3];
+//        students[0] = new Student();
+//        students[1] = new Student();
+//        students[2] = new Student();
+       //  Student[] students = new Student[]{new Student(),new Student(),new Student()};
+        Student[] students ={new Student(),new Student(),new Student()};
+        students[0].name = "Raj";
+        students[1].name = "John";
+        students[2].name = "Anita";
+        System.out.println("Student 1: " + students[0].name);
+        System.out.println("Student 2: " + students[1].name);
+        System.out.println("Student 3: " + students[2].name);
     }
 }
